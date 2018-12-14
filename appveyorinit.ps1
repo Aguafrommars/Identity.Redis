367ce4ffb6e1eb06e7551c18c89afe279ee64a53
Olivier Lefebvre <olivier.lefebvre@ne.ch>
2018-12-14 14:17:31 +0100

    ci: update ci config

---
+++ appveyorinit.ps1
@@ -0,0 +1,17 @@
+npm i -g semantic-release @semantic-release/exec @semantic-release/changelog @semantic-release/git @semantic-release/release-notes-generator @semantic-release/commit-analyzer 
+if (test-path ./nextversion.txt)
+{
+    Remove-Item ./nextversion.txt
+}
+semantic-release -b $env:APPVEYOR_REPO_BRANCH -d
+$nextversion = Get-Content ./nextversion.txt
+if (![string]::IsNullOrEmpty($env:GitVersion_PreReleaseLabel))
+{
+    $nextversion = "$nextversion-$env:GitVersion_PreReleaseLabel$env:GitVersion_CommitsSinceVersionSourcePadded"
+}
+
+dotnet restore
+appveyor UpdateBuild -Version $nextversion
+$builnumbersuffix = Get-Date -Format "mmddyyyy-HHmm"
+$builnumber = "$builnumber-$builnumbersuffix"
+appveyor SetVariable -Name APPVEYOR_BUILD_NUMBER -Value $builnumber
