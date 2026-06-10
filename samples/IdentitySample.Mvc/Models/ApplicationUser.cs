2384010001aa8e1e981797d3952bcd3d9999fc71
Olivier Lefebvre <LefebvreO@ne.ch>
2026-06-10 14:12:51 +0200

    fix: update packages

--- samples/IdentitySample.Mvc/Models/ApplicationUser.cs
+++ samples/IdentitySample.Mvc/Models/ApplicationUser.cs
@@ -1,5 +1,5 @@
 // Project: aguacongas/Identity.Redis
-// Copyright (c) 2025 @Olivier Lefebvre
+// Copyright (c) 2026 @Olivier Lefebvre
 using System;
 using System.Collections.Generic;
 using System.Linq;
